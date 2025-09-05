890ed42c7873cdb6872c3f9e6d54215202c3e7f0
AreejFahimm <areejfahim05@gmail.com>
2025-09-06 02:06:24 +0500

    mainpage

---
+++ app/src/main/java/com/AreejFahim/i230504/MainActivity5.kt
@@ -0,0 +1,15 @@
+package com.AreejFahim.i230504
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.mainpage)
+
+
+    }
+}
