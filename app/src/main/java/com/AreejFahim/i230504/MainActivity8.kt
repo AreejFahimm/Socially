b5f16d1e248eaf03a9d8e2648a9a5aae6d038f6c
AreejFahimm <areejfahim05@gmail.com>
2025-09-06 20:07:38 +0500

    callscreen,highlight screen

---
+++ app/src/main/java/com/AreejFahim/i230504/MainActivity8.kt
@@ -0,0 +1,14 @@
+package com.AreejFahim.i230504
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity8 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.highlightsscreen)
+
+    }
+}
