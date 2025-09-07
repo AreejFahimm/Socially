39b46a262b2aabea03aa58bbdee373110f2a27fb
AreejFahimm <areejfahim05@gmail.com>
2025-09-07 23:46:49 +0500

    camerascreen,editstoryscreen,mystoryscreen,profilescreen(half)

---
+++ app/src/main/java/com/AreejFahim/i230504/MainActivity12.kt
@@ -0,0 +1,15 @@
+package com.AreejFahim.i230504
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity12 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.camerascreen)
+    }
+}
