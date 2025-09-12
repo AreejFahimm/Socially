83834a1c991e231aac6e4655a722bdcff83349f8
AreejFahimm <areejfahim05@gmail.com>
2025-09-12 22:48:14 +0500

    Inboxscreen,Topsearchscreen

---
+++ app/src/main/java/com/AreejFahim/i230504/MainActivity17.kt
@@ -0,0 +1,13 @@
+package com.AreejFahim.i230504
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity17 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.topsearchscreen)
+    }
+}
